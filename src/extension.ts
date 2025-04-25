a01c20c672efde59def0f6fa4175802c13b03c9b
theBGuy <60308670+theBGuy@users.noreply.github.com>
2025-04-25 18:16:17 -0400

    [chore] Add biome & remove eslint

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
 import ts from "typescript";
+import * as vscode from "vscode";
 
 function findStyleSheetCreateDeclarations(
   sourceFile: ts.SourceFile
